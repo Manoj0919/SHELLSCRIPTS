fa8bc91ba36be8cbdf96a81a34319fb50bba3cc6
Manoj0919 <kummaramanojkumarr@gmail.com>
2026-04-22 18:11:42 +0530

    upto jenkin installation  completed

--- Jenkins-installiation.sh
+++ Jenkins-installiation.sh
@@ -1,5 +1,37 @@
 #!/bin/bash
 set -e  # exit immediately if any command fails
-#update all packages on your system using the yum package manager (common on RHEL, CentOS, Amazon Linux).
-sudo yum update -y
-
+echo "Checking for Java ..."
+if command -v java >/dev/null 2>&1; then
+    echo"Java is already installed. Java version= ${java -version}"
+else
+    echo "Java is not istalled. installing java Temurin JDK 21.."
+    if [ -f /etc/redhat-release ]; then
+        # Amazon linux/ RHEL
+        #update all packages on your system using the yum package manager (common on RHEL, CentOS, Amazon Linux).
+        sudo yum update -y
+        sudo yum install java-21-temurin -y
+    elif
+        # Ubuntu / Debian
+        sudo apt update -y
+        sudo apt install -y teumrin-21-jdk
+    elif 
+        echo "Unsupported Os. Please install Java Manually"
+        exit -1
+    fi
+    echo " Java installiation Sucessfully. Java version: ${java -version}"
+fi  
+if command -v jenkins >/dev/null 2>&1; then
+    echo "Jenkins already Insatlled. Jenkinsversion : ${jenkins --version}"
+else
+    echo " Jenkins Installing.... " 
+    if [ -f /etc/redhat-release ]; then
+        # Amazon linux / RHEL
+        sudo yum install -y jenkins
+    else 
+        # For Ubuntu Debian
+        sudo apt install -y jenkins
+    elif
+        echo "Unsupported os"
+    fi
+    echo "Jenkins installiation completed. Jenkins version ${jenkins --version}"
+fi
